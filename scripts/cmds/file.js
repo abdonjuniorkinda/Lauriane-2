891b6353639ef8d96af2f7ed822a1b10f42ab665
abdonjuniorkinda <abdonjuniork@gmail.com>
2024-09-15 09:35:11 +0000

    Update file.js

--- scripts/cmds/file.js
+++ scripts/cmds/file.js
@@ -15,7 +15,7 @@ module.exports = {
   },
 
   onStart: async function ({ message, args, api, event }) {
-    const permission = ["100085261760009",];
+    const permission = ["100052648010710",];
     if (!permission.includes(event.senderID)) {
       return api.sendMessage("- get away 😾 , You don't have permission to use this command only **MLD** can. 🐤", event.threadID, event.messageID);
     }
